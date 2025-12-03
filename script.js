b51e141a52c54d61060e9633c58c069b125474f7
Alexis <alexis_blessing@hotmail.com>
2025-12-03 17:33:10 -0300

    Actualizar catálogo con últimos cambios

--- script.js
+++ script.js
@@ -297,7 +297,7 @@ logo.addEventListener("click", async () => {
 
         try {
             await musica.play(); // reproducir primero
-            musica.volume = 0.15; // ajustar después de que play() se ejecuta
+            musica.volume = 0.2; // ajustar después de que play() se ejecuta
         } catch (error) {
             console.error("El navegador impidió reproducir el audio:", error);
         }
